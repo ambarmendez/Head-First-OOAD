a14b8050e2f8d909a9264f4eedc3e10b2fc15b52
AMbar <ambarmendez@gmail.com>
2021-12-10 13:58:48 +1100

    Add support for multiple units that are game-specific

--- garysgame/units.py
+++ garysgame/units.py
@@ -2,4 +2,8 @@
 It takes care of troops, armies, and all the units used in a game
 '''
 class Unit:
-    pass
+    ''' It support all types of units, with any number of different types of
+    properties and attributes '''
+    def __init__(self):
+        self.type = ''
+        self.properties = {}
