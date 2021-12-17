fe0ea77770ec05f23d654cd6a806c5035ca079ad
AMbar <ambarmendez@gmail.com>
2021-12-17 12:35:00 +1100

    Keep COMMONALITY-focused solution

---
+++ garysgame/tests.py
@@ -0,0 +1,58 @@
+from units import Unit
+
+
+class UnitTester:
+    ''' Each test focuses on a single piece of functionality, that might involve
+    one method, or several methods. Each test method has different parameters,
+    since each method is testing different things in the Unit class '''
+    def test_type(self, unit, type, expected_output_type):
+        print('\nTesting setting/getting the type property.')
+        unit.type = type
+        output_type = unit.type
+        if expected_output_type == output_type:
+            print('Test passed')
+        else:
+            print('Test failed:', output_type, "didn't match", expected_output_type)
+
+    def test_unit_specific_property(self, unit, property_name, input_value, expected_output_value):
+        ''' It tests one piece of functionality, setting a property, but it takes
+        two methods. You can't test setting a property's value, which uses
+        set_property(), without getting that property's value as well, by using
+        get_property() '''
+        print('\nTesting setting/getting a unit-specific property.')
+        unit.set_property(property_name, input_value)
+        output_value = unit.get_property(property_name)
+        if expected_output_value == output_value:
+            print('Test passed')
+        else:
+            print('Test failed:', output_value, "didn't match", expected_output_value)
+
+    def test_change_property(self, unit, property_name, input_value, expected_output_value):
+        ''' This assumes you've set the starting state correctly... otherwise, it will
+        ALWAYS fail '''
+        print("\nTesting changing an existing property's value.")
+        unit.set_property(property_name, input_value)
+        output_value = unit.get_property(property_name)
+        if expected_output_value == output_value:
+            print('Test passed')
+        else:
+            print('Test failed:', output_value, "didn't match", expected_output_value)
+
+    def test_non_existent_property(self, unit, property_name):
+        ''' This shows that we're not just dealing with happy paths... it shows Unit deals
+        with uses that are outside of the norm '''
+        print("\nTesting getting a non-existent property's value.")
+        output_value = unit.get_property(property_name)
+        if output_value is None:
+            print('Test passed')
+        else:
+            print('Test failed with value of', expected_output_value)
+
+
+if __name__ == '__main__':
+    tester = UnitTester()
+    unit = Unit(1000)
+    tester.test_type(unit, 'infantry', 'infantry')
+    tester.test_unit_specific_property(unit, 'hitPoints', 25, 25)
+    tester.test_change_property(unit, 'hitPoints', 15, 15)
+    tester.test_non_existent_property(unit, 'strength')
--- garysgame/units.py
+++ garysgame/units.py
@@ -2,7 +2,40 @@
 It takes care of troops, armies, and all the units used in a game
 '''
 class Unit:
-    ''' It support all types of units, with any number of different types of
-    properties and attributes '''
-    def __init__(self):
-        self.properties = {}
+    ''' It supports all types of units by having type, id, name, and weapons as
+    common properties share among all units. Any number of different attributes
+    that varies among each game-specific requirement goes inside the general
+    properties '''
+    def __init__(self, id):
+        self._id = id
+        self.type = ''
+        self.name = ''
+        self.weapons = None
+        self.properties = None
+
+    @property
+    def id(self):
+        return self._id
+
+    def add_weapon(self, weapon):
+        ''' It waits until there is a need for a weapons list to instantiate a
+        new list. That saves a little bit of memory, especially when there may
+        be a thousands of units '''
+        if self.weapons is None:
+            self.weapons = []
+        self.weapons.append(weapon)
+
+    def set_property(self, property, value):
+        ''' Just like weapons list, a dictionary is not created until it is nedeed '''
+        if self.properties is None:
+            self.properties = {}
+        self.properties[property] = value
+
+    def get_property(self, property):
+        ''' Since properties might not be initialized, there is an extra check before
+        looking up a property's value '''
+        if self.properties is None:
+            return None
+        if property in self.properties:
+            return self.properties[property]
+        return None
